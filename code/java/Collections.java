957896af7c17c648b6a251e610eaba84bbc149b5
Dereck Rasmussen <dereckrx@gmail.com>
2020-07-11 14:44:03 -0700

    More java collections

--- code/java/Collections.java
+++ code/java/Collections.java
@@ -1,12 +1,39 @@
-import java.util.List;
+import java.util.*;
 
 public class Collections {
-    public static void print(Object obj) {
+    public static void p(Object obj) {
         System.out.println(obj);
     }
+
     public static void main(String[] args) {
-        print("Hello, World 2");
-        var empty = List.of();
-        print(empty);
+      collections();
+    }
+
+  // Using Java 9 Factory of() method
+  // In Java,
+  public static void collections() {
+
+      // Use .of() with List, Map, or Set
+      var map = Map.of('a', 1, 'b', 2);
+      var list = List.of(1, 2, 3);
+      var set = Set.of(1, 2, 3);
+
+      p(set);
+      p(map);
+      p(list);
+
+      // Immutable empty
+      var empty = List.of();
+
+      List<String> arrayList = new ArrayList<>(
+          Arrays.asList("Geeks", "For", "Geeks"));
+      p(arrayList);
+
+      List<String> list2 = list1.stream().skip(1).collect(Collectors.toList())
+      p(list2);
+
+      //https://www.baeldung.com/java-immutable-list
+      //https://www.baeldung.com/java-combine-multiple-collections
+
     }
 }
