2de950f3c2fdc7cb1b55ccde20e3a07b937b7618
Pivotal <pivotal@Pivotals-MacBook-Pro-2.local>
2020-07-31 19:48:17 -0700

    Save notes

--- code/java/javaNotes/src/main/java/javaNotes/firstClassFunctions/FunctionTypes.java
+++ code/java/javaNotes/src/main/java/javaNotes/firstClassFunctions/FunctionTypes.java
@@ -5,22 +5,10 @@ import java.util.function.*;
 
 public class FunctionTypes {
 
-    public interface F {
-        public void apply();
-    }
-
-    public interface F0<R> {
-        public R apply();
-    }
-
-    public interface F1<T,R> {
-        public R apply(T t);
-    }
-
-    public interface F2<T, U, R> {
-        public R apply(T t, U u);
-    }
-
+    public interface F extends Runnable {}
+    public interface F0<T> extends Supplier<T> {}
+    public interface F1<T,R> extends Function<T,R> {}
+    public interface F2<T, U, R> extends BiFunction<T,U,R> {}
     public interface F3<T, U, V, R> {
         public R apply(T t, U u, V v);
     }
@@ -59,6 +47,5 @@ public class FunctionTypes {
         Predicate<Integer> isOne = number -> number == 1;
         Function<Integer, Boolean> isOne2 = number -> number == 1;
         F1<Integer, Boolean> isOne3 = number -> number == 1;
-
     }
 }
