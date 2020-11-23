c25af49a470c1e6b3604bb7818b823c739c4481b
Dereck Rasmussen <dereckrx@gmail.com>
2020-11-22 21:40:52 -0800

    Save notes

--- code/javascript/smallest-test-library.js
+++ code/javascript/smallest-test-library.js
@@ -1,9 +1,4 @@
-const eq = (a, b) => ((as, bs, p, f) => (as === bs ? p('.') : p(`FAIL: ${as} === ${bs} - ${f()}`)))(
-  JSON.stringify(a),
-  JSON.stringify(b),
-  console.log,
-  () => (new Error()).stack.split('\n')[4].split('/').slice(-1)[0].trim(),
-);
+const eq = require('./eq');
 
 eq(1, 1);
 eq(1, 2); // FAIL: 1 === 2
