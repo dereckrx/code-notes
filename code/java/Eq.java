8f3f8bfc1b9aad332f7d1dea71491d9d5ab52a7a
Dereck Rasmussen <dereckrx@gmail.com>
2020-11-25 18:55:16 -0800

    Save notes

--- code/java/Eq.java
+++ code/java/Eq.java
@@ -6,7 +6,7 @@ import java.util.function.Consumer;
 
 class Eq {
   public static <T> void eq(T a, T b) {
-    if (a == null && b == null || a.equals(b)) {
+    if (a == null && b == null || (a != null && a.equals(b))) {
       System.out.println(".");
     } else {
       System.out.println("FAIL: "+a+" == "+b+"\t"+(new Exception()).getStackTrace()[1]);
@@ -16,6 +16,7 @@ class Eq {
   public static void main(String args[]) {
     eq(null, null);
     eq(null, 1);
+    eq(1, null);
     eq(1, 1);
     eq(1, 2);
     eq(1.0, 1);
--- code/javascript/eq.js
+++ code/javascript/eq.js
@@ -1,8 +1,8 @@
-const eq = (a, b) => ((as, bs, p, f) => (as === bs ? p('.') : p(`FAIL: ${as} === ${bs} - ${f()}`)))(
+const eq = (a, b) => ((as, bs, p, f) => (as === bs ? p('.') : p(`FAIL: ${as} === ${bs}\t${f()}`)))(
   JSON.stringify(a),
   JSON.stringify(b),
   console.log,
-  () => (new Error()).stack.split('\n')[4].split('/').slice(-1)[0].trim(),
+  () => (new Error()).stack.split('\n')[4].match(/\/([^/]+):\d+\)/)[1],
 );
 
 module.exports = eq;
