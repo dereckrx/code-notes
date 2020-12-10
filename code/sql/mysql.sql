7c2dddfb8175c8a6e3a72b9fee6aab03370db2ad
Dereck Rasmussen <drasmussen@pivotal.io>
2020-12-10 09:16:42 -0800

    Save notes

--- code/sql/mysql.sql
+++ code/sql/mysql.sql
@@ -1,11 +1,21 @@
+
+
 -- Syntax
 -- Backticks for table and column identifiers: `Users`, `username`
--- Use single quotes for string and date values 'hello world', '2001-01-01 00:00:00' (double quotes are supported, but single is prefered)
+-- Single quotes for string and date values 'hello world', '2001-01-01 00:00:00' (double quotes are supported, but single is prefered)
 
+-- command line 
+-- mysql -u root -p --database=example_app -h 127.0.0.1
+-- show databases; show tables; use <database_name>; 
+
+--- Pretty Print result
+select * from x\G; 
 
 -- Update Table
-ALTER TABLE `OrderForm` CHANGE payback_start_month payback_start_month int(11) NOT NULL;
+ALTER TABLE `Todos` CHANGE payback_start_month payback_start_month int(11) NOT NULL;
 
 
-UPDATE `OrderForm`  SET payback_start_month=benefit_start_month + 1, payback_start_year=benefit_start_year 
-WHERE payback_start_month IS NULL AND payback_start_year IS NULL AND o1.id=o2.id;
+UPDATE `Todos`  SET payback_start_month=benefit_start_month + 1, payback_start_year=benefit_start_year 
+WHERE payback_start_month IS NULL AND payback_start_year IS NULL;
+
+ALTER TABLE `TodoLists` DROP COLUMN estimated_total_credit_override;
